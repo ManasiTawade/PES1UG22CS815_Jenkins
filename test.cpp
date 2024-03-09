aab72f681b8a07c107239225753b69605614e010
ManasiTawade <162109434+ManasiTawade@users.noreply.github.com>
2024-03-09 14:23:50 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
-  cout << "webhook up and running"
+  cout << "Hello, World\nPES1UG222CS815";
   return 0;
 }
